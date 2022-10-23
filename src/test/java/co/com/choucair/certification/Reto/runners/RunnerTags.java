a690a65134b8d6efa9292a550abd6e059d8f0aa2
oscar <oscarmauel4502@outlook.com>
2022-10-23 18:27:47 -0500

    commit inicial

---
+++ src/test/java/co/com/choucair/certification/Reto/runners/RunnerTags.java
@@ -0,0 +1,15 @@
+package co.com.choucair.certification.Reto.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/demo.feature",
+tags = "@tag1",
+glue = "co.com.choucair.cetification.Reto.stepdefinitions",
+snippets = SnippetType.CAMELCASE)
+
+public class RunnerTags {
+}
